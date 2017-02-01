7410e0d41aa71fd52e4f3a833c29aeeb12c3ea56
Adam Smith <AlarmingSmock@Adams-MacBook-Pro.local>
2017-01-31 23:39:54 -0500

    Added the metadate to the converted playlist. It should work now.

--- PLChauffer/chauffer.py
+++ PLChauffer/chauffer.py
@@ -18,11 +18,17 @@ reverse_slash_direction = True
 # Open File Read In
 playlistFile = open('../Playlists/Favorites.m3u', 'r')
 playlist = playlistFile.readlines()
+playlistFile.close()
 
 song_paths_bare = []
 count = 0
+
+new_playlist = open('New_{}'.format(playlist_name), 'w')
+#printlist = ''
 for line in playlist:
 	if line[0] == '#':
+		new_playlist.write(line)
+		#printlist = printlist.join(line)
 		continue
 	#print '{} {}'.format(line,count)
 	#print line.rstrip()
@@ -35,13 +41,15 @@ for line in playlist:
 			found = True
 		else:
 			song_path_old.popleft()
-	song_paths_bare.append(song_path_old)
 
-new_playlist = open('New_{}'.format(playlist_name), 'w')
-for path in song_paths_bare:
-	str_path = '/'.join(path)
+	str_path = '/'.join(song_path_old)
 	str_path = '{}/{}'.format(new_playlist_path,str_path)
 	if reverse_slash_direction:
 		str_path = str_path.replace('/','\\')
+		#printlist = printlist.join(line)
 	new_playlist.write(str_path)
+	#printlist = printlist.join(line)
+
+#print printlist	
+
 	
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,6 +1,14 @@
 #For Progress
 
 Jan 31 2017
+It turns out in m3u format the #EXT info line is VITAL to the playlist working.
+It contains critical metadata including runtime! title and artist.
+
+I'll add those lines as well and things should work :)
+
+_____________________________________________________________________
+Jan 30 2017
 I got the thing doing what I wanted it to. It converts the path and everything but the files CANT BE RUN IN VLC.
 
 There's more info in the logs but I haven't found them.
+
